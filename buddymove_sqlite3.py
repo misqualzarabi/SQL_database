2fc30439cd1883901e2a0e77667fff1323bf257a
Misqual Zarabi <50719263+misqualzarabi@users.noreply.github.com>
2020-05-25 15:06:31 +0100

    changed the connction name

--- buddymove_sqlite3.py
+++ buddymove_sqlite3.py
@@ -2,7 +2,7 @@ import pandas as pd
 buddy = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/00476/buddymove_holidayiq.csv')
 
 import sqlite3
-conn = sqlite3.connect('buddymove.db')
+conn = sqlite3.connect('buddymove_holidayiq.sqlite3')
 c = conn.cursor()
 
 '''c.execute('CREATE TABLE reviews' (buddy.columns))
